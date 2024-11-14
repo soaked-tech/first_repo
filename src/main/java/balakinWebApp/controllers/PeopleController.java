99501e8fcf0e7d05762d966f39a87f4cec0c4a41
Vladimir Balakin <balakinvv00313@gmail.com>
2024-11-14 12:58:25 +0300

    PreparedStatement SQL

--- src/main/java/balakinWebApp/controllers/PeopleController.java
+++ src/main/java/balakinWebApp/controllers/PeopleController.java
@@ -10,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
+
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
@@ -21,13 +23,13 @@ public class PeopleController {
     }
 
     @GetMapping()
-    public String index(Model model) {
+    public String index(Model model) throws SQLException {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
 
     @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model) {
+    public String show(@PathVariable("id") int id, Model model) throws SQLException {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
@@ -39,7 +41,7 @@ public class PeopleController {
 
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid Person person,
-                         BindingResult bindingResult) {
+                         BindingResult bindingResult) throws SQLException {
         if (bindingResult.hasErrors())
             return "people/new";
 
@@ -48,14 +50,14 @@ public class PeopleController {
     }
 
     @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id) {
+    public String edit(Model model, @PathVariable("id") int id) throws SQLException {
         model.addAttribute("person", personDAO.show(id));
         return "people/edit";
     }
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person,
-                         BindingResult bindingResult, @PathVariable("id") int id) {
+                         BindingResult bindingResult, @PathVariable("id") int id) throws SQLException {
         if (bindingResult.hasErrors())
             return "people/edit";
 
@@ -64,7 +66,7 @@ public class PeopleController {
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
+    public String delete(@PathVariable("id") int id) throws SQLException {
         personDAO.delete(id);
         return "redirect:/people";
     }
--- src/main/java/balakinWebApp/dao/PersonDAO.java
+++ src/main/java/balakinWebApp/dao/PersonDAO.java
@@ -30,9 +30,9 @@ public class PersonDAO {
         }
     }
 
-    public List<Person> index(){
+    public List<Person> index() throws SQLException {
         List<Person> people = new ArrayList<>();
-        try {
+
             Statement statement = connection.createStatement();
             String SQL = "SELECT * FROM Person";
             ResultSet resultSet = statement.executeQuery(SQL);
@@ -47,45 +47,55 @@ public class PersonDAO {
 
                 people.add(person);
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
 
         return people;
     }
 
-    public Person show(int id) {
-        return null;
-//       return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    public Person show(int id) throws SQLException {
+        Person person = null;
+
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("SELECT * FROM Person WHERE id=?");
+        preparedStatement.setInt(1,id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        person = new Person();
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setEmail(resultSet.getString("email"));
+        person.setAge(resultSet.getInt("age"));
+
+        return person;
     }
 
-    public void save(Person person){
-//        person.setId(++PEOPLE_COUNT);
-//        people.add(person);
+    public void save(Person person) throws SQLException {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO Person VALUES (1,?,?,?)");
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
 
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" +
-                    person.getName() +  "'," + person.getAge() +
-                    ",'" + person.getEmail() + "')";
-
-            statement.executeUpdate(SQL);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+            preparedStatement.executeUpdate();
     }
 
-    public void update(int id,Person updatedPerson){
-//        Person personTiBeUpdated = show(id);
-//
-//        personTiBeUpdated.setName(updatedPerson.getName());
-//        personTiBeUpdated.setAge(updatedPerson.getAge());
-//        personTiBeUpdated.setEmail(updatedPerson.getEmail());
+    public void update(int id,Person updatedPerson) throws SQLException {
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id = ?");
+
+        preparedStatement.setString(1, updatedPerson.getName());
+        preparedStatement.setInt(2, updatedPerson.getAge());
+        preparedStatement.setString(3, updatedPerson.getEmail());
+        preparedStatement.setInt(4,id);
+
+        preparedStatement.executeUpdate();
     }
 
-    public void delete(int id){
-  //      people.removeIf(p -> p.getId() == id);
+    public void delete(int id) throws SQLException {
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("DELETE FROM Person WHERE id=?");
 
+        preparedStatement.setInt(1,id);
 
+        preparedStatement.executeUpdate();
     }
 }
